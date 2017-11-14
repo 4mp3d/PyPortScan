f86244931413655ef54e7be6bb6483ea4d1c747f
4mp3d <asieting@hagerty.com>
2017-11-14 09:50:44 -0500

    More cleanup

--- utils.py
+++
@@ -1,9 +0,0 @@
-def timefunc(func):
-	def inner(*args, **kwargs):
-		start = timer()
-		results = func(*args, **kwards)
-		end = timer()
-		message = '{} took {} seconds'.format(func.__name__, end - start)
-		print(message)
-		return results)
-	return inner
